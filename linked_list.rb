0b78626e346c5627bdf1e00461ac702e652698d7
scottrobel <robelrain@gmail.com>
2019-11-06 19:11:29 -0500

    added append method

--- linked_list.rb
+++ linked_list.rb
@@ -1,6 +1,18 @@
-Node = Struct.new(:data,:link)
+Node = Struct.new(:data,:next)
 class LinkedList
   def initialize
     @head = nil
   end
+  def append(data)
+    temp = @head
+    if(@head == nil)
+      @head = Node.new(data)
+    else
+      until(temp.next == nil) do
+        temp = temp.next
+      end
+      temp.next = Node.new(data)
+    end
+  end
+
 end
