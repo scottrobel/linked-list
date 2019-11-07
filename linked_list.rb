6db48c2b50ee41d65965399f717d6d8d3190d689
scottrobel <robelrain@gmail.com>
2019-11-06 20:12:23 -0500

    added size method

--- linked_list.rb
+++ linked_list.rb
@@ -20,8 +20,18 @@ class LinkedList
   def prepend(value)
     @head = Node.new(value, @head)
   end
-  
+
+  def size
+    temp = @head
+    size = 0
+    until temp.nil?
+      size += 1
+      temp = temp.next
+    end
+    size
+  end
 end
 my_linked_list = LinkedList.new
 my_linked_list.append('some_value')
 my_linked_list.prepend('some_value')
+puts my_linked_list.size
