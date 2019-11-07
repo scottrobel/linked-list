53d4f229607ef2550cdb5fc16434b7a3b743852f
scottrobel <robelrain@gmail.com>
2019-11-07 15:54:50 -0500

    fixed error in the pop method of linked list so it does not throw an error when the list is only one node long

--- linked_list.rb
+++ linked_list.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# a node has a value and a link
 Node = Struct.new(:value, :next)
 # created a linked list between nodes
 class LinkedList
@@ -10,13 +11,13 @@ class LinkedList
   end
 
   def tail
-    temp = @head
+    temp = head
     temp = temp.next until temp.next.nil?
     temp
   end
 
   def append(value)
-    if @head.nil?
+    if head.nil?
       prepend(value)
     else
       tail.next = Node.new(value)
@@ -24,11 +25,11 @@ class LinkedList
   end
 
   def prepend(value)
-    @head = Node.new(value, @head)
+    @head = Node.new(value, head)
   end
 
   def size
-    temp = @head
+    temp = head
     size = 0
     until temp.nil?
       size += 1
@@ -38,7 +39,7 @@ class LinkedList
   end
 
   def at(index)
-    temp = @head
+    temp = head
     index.times do
       temp = temp.next
     end
@@ -46,17 +47,21 @@ class LinkedList
   end
 
   def pop
-    return if @head.nil?
+    return if head.nil?
 
-    temp = @head
-    temp = temp.next until temp.next.next.nil?
-    temp.next = nil
+    if head.next.nil?
+      @head = nil
+    else
+      temp = head
+      temp = temp.next until temp.next.next.nil?
+      temp.next = nil
+    end
   end
 
   def contains?(value)
-    return false if @head.nil?
+    return false if head.nil?
 
-    temp = @head
+    temp = head
     until temp.next.nil?
       return true if temp.value == value
 
@@ -66,9 +71,9 @@ class LinkedList
   end
 
   def to_s
-    return 'nil' if @head.nil?
+    return 'nil' if head.nil?
 
-    temp = @head
+    temp = head
     data_string = ''
     until temp.nil?
       data_string += "( #{temp.value} ) -> "
@@ -82,25 +87,46 @@ class LinkedList
     prepend(value) if index.zero?
     return if head.nil? || index.zero?
 
-    prev = nil
-    cur = @head
-    index.times do
-      prev = cur
-      cur = cur.next
-    end
+    prev, cur = get_prev_and_cur_nodes(index)
     prev.next = Node.new(value, cur) unless cur.nil?
   end
 
   def remove_at(index)
-    prepend(value) if index.zero?
+    @head = head.next if index.zero?
     return if head.nil? || index.zero?
 
+    prev, cur = get_prev_and_cur_nodes(index)
+    prev.next = cur.next unless cur.nil?
+  end
+
+  private
+
+  def get_prev_and_cur_nodes(index)
     prev = nil
-    cur = @head
+    cur = head
     index.times do
       prev = cur
       cur = cur.next
     end
-    prev.next = cur.next unless cur.nil?
+    [prev, cur]
   end
 end
+
+my_linked_list = LinkedList.new
+puts my_linked_list
+my_linked_list.prepend(5)
+puts my_linked_list
+my_linked_list.append(4)
+puts my_linked_list
+my_linked_list.insert_at(1, 'val')
+puts my_linked_list
+my_linked_list.remove_at(1)
+puts my_linked_list
+puts my_linked_list.contains?(5)
+puts my_linked_list.size
+puts my_linked_list.at(1)
+puts my_linked_list
+my_linked_list.pop
+puts my_linked_list
+my_linked_list.pop
+puts my_linked_list
